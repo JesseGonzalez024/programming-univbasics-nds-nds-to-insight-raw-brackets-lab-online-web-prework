923efc2303ddf4c4617b6bc4dc92b77b9c3b5a78
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-03 21:59:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,28 +5,18 @@ require 'directors_database'
 
 pp directors_database
 
+
+
+
 new_hash = {name: value}
 
 def directors_totals(nds)
 
-  pp nds
-  
-  grand_total = 0
-  row_index = 0
-  inner_index = 0
-  
+  index1 = 0
   
   while nds.each do
-    new_hash[] = nds[row_index][:name]
+  new_hash = nds[inex1]
     
-   
-   
-    while row_index < nds.length do
-  column_index = 0
-  while column_index < nds[row_index].length do
-    inner_len = vm[row_index][column_index].length
-    inner_index = 0
-    while inner_index < inner_len do
      
       
       
